7dfb4d777de3ed194c4ada0817f3a02b84088b56
Yuliya Samuilik <Yuliya_Samuilik@epam.com>
2018-10-09 12:10:34 +0300

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <button @click="click">
+    {{ buttonText }}
+  </button>
+
+</template>
+
+
+<script>
+
+  import Button from './Button.vue';
+
+  export default {
+    name: 'Button',
+    data () {
+      return {
+        buttonText: 'CLICK Button component'
+      }
+    },
+    methods: {
+      click () {
+        alert('I am Button component :)')
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+
+  button {
+    width: 200px;
+    height: 100px;
+    background: crimson;
+    cursor: pointer;
+    font-size: 20px;
+  }
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <footer>
+    <p>  {{ footerText }} </p>
+    <Button/>
+  </footer>
+
+</template>
+
+
+<script>
+
+  import Button from './Button.vue';
+
+  export default {
+    name: 'Footer',
+    components: {
+      Button
+    },
+    data () {
+      return {
+        footerText: 'Footer Component'
+      }
+    }
+  }
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 98%;
+    background: #41B883;
+
+    p {
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+  }
+
+</style>
